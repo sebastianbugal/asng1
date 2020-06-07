e91c03b8a10e70d7616296911cd3e483c7259c83
Sebastian Bugal <seb1001@SebastiansMBP2.hitronhub.home>
2020-06-07 00:45:47 -0700

    csv upload works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 1300
+const PORT = process.env.PORT || 2000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
